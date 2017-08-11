9864e096fc52c64c50b4adc99013e324d654307d
Jerson Gomez <jermetrix@gmail.com>
2017-08-11 14:11:49 -0500

    Spring boot

---
+++ src/main/java/com/levitsof/profesoresLevitsof/ProfesoresLevitsofApplication.java
@@ -0,0 +1,12 @@
+package com.levitsof.profesoresLevitsof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfesoresLevitsofApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfesoresLevitsofApplication.class, args);
+	}
+}
---
+++ src/test/java/com/levitsof/profesoresLevitsof/ProfesoresLevitsofApplicationTests.java
@@ -0,0 +1,16 @@
+package com.levitsof.profesoresLevitsof;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProfesoresLevitsofApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
